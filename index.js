57b4a1f84db802c985cbbced04d5b2a8710c5093
Defne Celikoyar <defne.celikoyar@gmail.com>
2021-11-27 17:29:11 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+]
+
+
+function superbowlWin(array){
+    for (const item of array){
+        if (item.result === "W") {
+            return item.year}
+    };
+}
 
